eca1cf301e1e58df2456a046e901395ab4520e1c
Анастасия <89359100+Asenka90@users.noreply.github.com>
2022-10-13 16:32:32 +0300

    Add files via upload

---
+++ data_generate.py
@@ -0,0 +1,45 @@
+import random as rnd
+
+MEN_NAMES_CNT = 13
+MEN_LASTN_CNT = 13
+WOMEN_NAMES_CNT = 11
+WOMEN_LASTN_CNT = 12
+
+keys_val = ('name', 'name_last', 'phone')
+
+# генерация даных
+names_set = (   'Андрей', 'Сергей', 'Дмитрий', 'Олег', 'Семен', 'Иван',
+                'Николай', 'Михаил', 'Петр', 'Федор', 'Павел', 'Владимир', 'Владислав',
+                'Мария', 'Анна', 'Елена', 'Светлана', 'Ольга', 'Тамара',
+                'Татьяна', 'Евгения', 'Оксана', 'Яна', 'Жанна')
+
+lastn_set = (   'Иванов', 'Петров', 'Сидоров', 'Николаенко', 'Галушкин', 'Баширов',
+                'Семенов', 'Крупеня', 'Лобов', 'Павлов', 'Тимирязев', 'Тополев', 'Морева',
+                'Еремина', 'Иванова', 'Давыдова', 'Курочкина', 'Татьмина', 'Соколова',
+                'Решкина', 'Павлова', 'Сидорцова', 'Каманова', 'Толочко', 'Кашина')
+
+
+def data_gen(cnt_record: int = 20) -> dict:
+    result = []
+    print(cnt_record)
+    for i in range(cnt_record):
+        mw = rnd.randint(0, 1)   # м / ж
+        person = []
+
+        k = rnd.randint(0, MEN_NAMES_CNT) if mw == 1 else rnd.randint(
+            MEN_NAMES_CNT, MEN_NAMES_CNT+WOMEN_NAMES_CNT-1)
+        person.append(names_set[k])
+        
+        k=rnd.randint(0, MEN_LASTN_CNT) if mw == 1 else rnd.randint(
+            MEN_LASTN_CNT, MEN_LASTN_CNT+WOMEN_LASTN_CNT-1)
+        person.append(lastn_set[k])
+        
+        person.append(f'{rnd.randint(11111, 99999)}')
+
+        tmp = {}
+        for i in range(len(person)):
+            tmp[keys_val[i]] = person[i]
+
+        result.append(tmp)
+        
+    return result
---
+++ data_read_write.py
@@ -0,0 +1,50 @@
+# чтение данных из файла, в файл
+
+import os
+from turtle import pd
+# чтение из txt
+def read_txt(path: str = 'data.txt') -> list:
+    if not os.path.exists(path):
+        return {}
+    
+    result = {}
+    with open(path, 'r', encoding='utf-8') as data:
+        line = data.readline().replace('\n', '')
+        result = []
+
+        keys = line.split(' ')
+        line = data.readline().replace('\n', '')
+        while line != '':
+            values = line.split(' ')
+            tmp = {}
+            for i in range(len(keys)):
+                tmp[keys[i]] = values[i]
+            
+            result.append(tmp)
+            line = data.readline().replace('\n', '')
+
+    return result
+
+
+# запись в csv
+def save_csv(data: list, path: str = 'data.csv')-> list:
+
+    df = pd.DataFrame.from_dict(data)
+    df.to_csv(path, encoding='utf-8', index=False)
+
+    return data
+
+# запись в txt
+def save_txt(data: list, path: str = 'data.txt') -> list:
+    l_keys = list(data[0].keys())
+    lines = ' '.join(l_keys) + '\n'
+    for i in data:
+        for k in l_keys:
+            lines += i[k] + ' '
+        lines = lines[0:-1] + '\n'
+    lines = lines[0:-1]
+    
+    with open(path, 'w', encoding='UTF-8') as file:
+        file.writelines(lines)
+            
+    return data
